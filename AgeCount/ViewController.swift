3534c08d55e0d789196ee4458fa89517d7b2c1de
MacBook <macbook@macbooks-macbook-pro.local>
2016-02-24 01:14:42 +0200

    my app function is working good but i need to good design for my firs app :)

--- AgeCount/ViewController.swift
+++ AgeCount/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by MacBook on 23/02/16.
 //  Copyright © 2016 sgumus. All rights reserved.
-//
+//menderes mhl 329 sokak no:2 D=5 esenler istanbul
 
 import UIKit
 
@@ -12,10 +12,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var ageLabel: UILabel!
     
+    var timer = NSTimer()
+    @IBOutlet weak var artangunlabel: UILabel!
     @IBOutlet weak var myPicker: UIDatePicker!
     
     let dateFormatter = NSDateFormatter()
     let currentDate = NSDate()
+    var salise=0
 
      
    // let myAge = myDOB.age      // 45
@@ -28,27 +31,36 @@ class ViewController: UIViewController {
     var birtohMonth:Int = 0
     var birthOfday:Int = 0
     
+    var artangun:Int=0
+    
     @IBAction func ageCountShow(sender: AnyObject) {
     
       dateFormatter.dateFormat = "yyyy"
       dateOfBirthDaysCounting()
 
       print("MY Old:\(countYear())")
-        ageLabel.text=String(countYear())
+      //  artangunlabel.text=String(artangun*24*60*60*60)
+        ageLabel.text="\(String(countYear()))\n Years Old"
+        
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        countYear()
+        ageLabel.numberOfLines=0
+        ageLabel.text="\(String(countYear()))\n Years Old"
         let calendar = NSCalendar.currentCalendar()
         let components = calendar.components([.Day , .Month , .Year], fromDate: currentDate)
         
         currentYear=components.year
         currentMonty=components.month
         currentDay=components.day
-        print(currentYear)
-        print(currentMonty)
-        print(currentDay)
+        
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target:self, selector: Selector("updateCounter"), userInfo: nil, repeats: true)
+//        print(currentYear)
+//        print(currentMonty)
+//        print(currentDay)
         
         
     }
@@ -61,8 +73,9 @@ class ViewController: UIViewController {
         let currentdays=(currentYear*365)+(currentMonty*30)+(currentDay)
         let dateofBirthDays=(birthOfYear)+(birtohMonth)+(birthOfday)
         let manOld=(currentdays-dateofBirthDays)/365
-        let mandays=(currentdays-dateofBirthDays)-manOld*365
-        print("artan Gun \(mandays)")
+        artangun=(currentdays-dateofBirthDays)-manOld*365
+        salise=artangun*24*60*60*60
+        print("artan Gun \(artangun)")
         return manOld
     }
     
@@ -98,7 +111,13 @@ class ViewController: UIViewController {
         birthOfday=Int(yearPicker)!
         print(birthOfday)
         
+        countYear()
 
+    }
+    
+    
+    func updateCounter() {
+        artangunlabel.text = String(salise++)
     }
 }
 
